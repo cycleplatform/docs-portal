391a89891fc6bc9194cbf98cc0dcb3916dff2124
Christopher Aubuchon <caubuchon@petrichor.io>
2022-07-27 16:28:48 -0700

    hotfix stack typos

--- docs/stacks/reference/components/config/deploy/deploy.js
+++ docs/stacks/reference/components/config/deploy/deploy.js
@@ -1,9 +1,6 @@
-import React from 'react'
-
-
+import React from "react";
 
 // ❌  ✅
-
 
 export const ConstraintsExample = `{
   "node": {
@@ -13,24 +10,22 @@ export const ConstraintsExample = `{
     }
   },
   "containers": ["frontend", "api"]
-}`
-
+}`;
 
 export const ShutdownExample = `{
   "graceful_timeout": 45,
   "signals": ["SIGTERM", "SIGINT", "SIGHUP","SIGQUIT"]
-}`
+}`;
 
 export const StartupExample = `{
   "delay":  10
-}`
-
+}`;
 
 export const RestartExample = `{
   "condition": "always",
   "delay": 10,
   "max_attempts": 10
-}`
+}`;
 
 export const HealthCheckExample = `{
   "command": "healthcheck_script"
@@ -38,12 +33,11 @@ export const HealthCheckExample = `{
   "interval": 5,
   "timeout": 30,
   "restart": true
-}`
-
+}`;
 
 export const TelemetryExample = `{
   "retention": 35000,
   "interval": 30,
-  "webhook": "https://myurl.com/endpoint"
+  "webhook": "https://myurl.com/endpoint",
   "disable": false
-}`
+}`;
