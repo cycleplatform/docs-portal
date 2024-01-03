17df60a0f5bb16e0d0c81260000e4cb9744edeba
Christopher Aubuchon <aubuchcl@gmail.com>
2024-01-03 08:23:13 -0800

    build works on m series

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-docker build -t cycleplatform/portal-docs:latest . 
+docker buildx build --platform linux/amd64 -t cycleplatform/portal-docs:latest . 
 
 docker push cycleplatform/portal-docs
